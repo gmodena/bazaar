7083a9095ab08e09aa3868e206cb0c23ded8f2cc
Gabriele Modena <gm@nowave.it>
2021-01-24 21:10:25 +0100

    Update uuid version

--- extension/js/auction.es6.js
+++ extension/js/auction.es6.js
@@ -1,4 +1,4 @@
-const uuidv4 = require('uuid/v4');
+const { v4: uuidv4 } = require('uuid');
 const bidders = require("./bidders.es6")
 const storage = require("./storage.es6")
 
--- extension/js/storage.es6.js
+++ extension/js/storage.es6.js
@@ -1,4 +1,4 @@
-const uuidv4 = require('uuid/v4');
+const { v4: uuidv4 } = require('uuid');
 const PouchDB = require('pouchdb').default;
 
 const remoteDbHost = 'http://localhost:5984'
