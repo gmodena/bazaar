615a1ee89d44fa7ec63423ea775f450ea6a7485b
Gabriele Modena <gm@nowave.it>
2021-03-14 13:57:19 +0100

    Refactor popup UI (#9)
    * Refactor popup templating logic
    
    * Remove debug logging
    
    * Refactor data loaders
    
    * Report only prebid partners name

--- extension/popup/popup.es6.js
+++ extension/popup/popup.es6.js
@@ -2,100 +2,38 @@ const Events = require("../js/events.es6").Events
 const storage = require('../js/storage.es6')
 const numberOfBidders = 5 
 
-function setPopupData(data) {
-    /**
-    * 
-    * @param {*} data associative array in the form [["Bidder", n_bids]]
-    */
-    function toHtmlList(data) {
-        var html = '<ul>'
-        data.forEach(bidder => html += '<li>' + bidder[0] + '</li>' )
-        html += '</ul>'
-
-        return html
+function compare( a, b ) {
+    if ( a.value > b.value ){
+      return -1;
     }
-    if (data) {
-        try {
-            if  (! data.hasOwnProperty("bidders")) { throw "Received invalid data" } 
-            let prebidBidders = document.getElementById('prebid-bidders');
-            var message = "No Prebid.js adapter found on page"
-
-            if  (data["bidders"].length > 0) {
-                message = `Prebid partners on page: <br/>`
-                message += toHtmlList(data["bidders"]);
-            }
-            
-            prebidBidders.innerHTML += message
-        } catch(e) {
-            console.log(e)
-        }
+    if ( a.value < b.value ){
+      return 1;
     }
-    document.getElementById('prebid-stats-button')
-            .addEventListener('click', buildStatsUI);
+    return 0;
 }
 
+let Popup = function (options) {
+	this.elem = document.querySelector(options.selector);
+	this.data = options.data;
+	this.template = options.template;
+};
 
-function buildStatsUI() {
-    /**
-     * List of rows from pouchdb map/reduce query.
-     * @param {*} bidders { rows: [Object]}
-     */
-    function buildTopKUI(bidders, statsType) {
-        function toHtmlList(data) { 
-            console.log(data)
-            var html = '<ul>'
-            data.forEach(bidder => {
-                html += '<li>' + bidder.key + " " + bidder.value + '</li>'
-            })
-            return html
-        }
-        function compare( a, b ) {
-            if ( a.value > b.value ){
-              return -1;
-            }
-            if ( a.value < b.value ){
-              return 1;
-            }
-            return 0;
-        }
+Popup.prototype.render = async function () {
+    this.elem.innerHTML = await this.template(this.data);
+};
 
-        // 'rows' is part of the schema returned by pouchdb
-        if  (! bidders.hasOwnProperty("rows")) { throw "Invalid data" } 
-        
-        const data = bidders['rows'].sort(compare).slice(0, numberOfBidders)
-        let prebidStatsDiv = document.getElementById(statsType)
-        prebidStatsDiv.innerHTML += toHtmlList(data)
-    } 
-
-    function hideElements() {
-        document.getElementById('prebid-bidders').style.display = 'none'
-        document.getElementById('prebid-stats-button').style.display = 'none'
-    }
-
-    function showStats() {
-        const bidderStatsDiv = "prebid-bidder-stats"
-        const winnerStatsDiv = "prebid-winner-stats"
-    
-        document.getElementById(bidderStatsDiv).style.display = 'block'
-        document.getElementById(winnerStatsDiv).style.display = 'block'
-
-        storage.stats.getBiddersCount(numberOfBidders).then(function(result) {
-            buildTopKUI(result, bidderStatsDiv)
-        }).catch(function (err) {
-            console.log(err)
-        })
-    
-        storage.stats.getWinnersCount(numberOfBidders).then(function(result) {
-            buildTopKUI(result, winnerStatsDiv)
-        }).catch(function (err) {
-            console.log(err)
-        })
-    }
-
-    hideElements()
-    showStats()
+let StatsLoader = async function(fn, k) {
+    let data = await fn(k)
+    return data['rows'].sort(compare).slice(0, k)
 }
 
+let Bidders = async function(k) {
+    return StatsLoader(storage.stats.getBiddersCount, k)
+};
+
+let Winners = async function(k) {
+    return StatsLoader(storage.stats.getWinnersCount, k)
+};
 
 browser.tabs.query({active: true, lastFocusedWindow: true}).then(tabs => {
     browser.runtime.sendMessage({
@@ -103,6 +41,60 @@ browser.tabs.query({active: true, lastFocusedWindow: true}).then(tabs => {
         tabId: tabs[0].id,
         url: tabs[0].url
     }).then(response => {
-        setPopupData(response)
+        var bidders = []
+        if (response) {
+            bidders = response.bidders
+        }
+        new Popup({
+            selector: '#app',
+            data: {
+                heading: 'Prebid partners',
+                bidders: bidders
+            },
+            template: function (props) {
+                return `
+                    <h3>${props.heading}</h3>
+                    <ul>
+                        ${props.bidders.map(function (bidder) {
+                            return `<li>${bidder[0]}</li>`;
+                        }).join('')}
+                    </ul>`;
+            }
+        }).render()
+
+        document.getElementById('prebid-stats-button').addEventListener('click', function(){
+            new Popup({
+                selector: '#app',
+                data: {
+                    bidders: {
+                        heading: 'Bidder stats',
+                        data: Bidders
+                    
+                    },
+                    winners: {
+                        heading: 'Winner stats',
+                        data: Winners
+                    }
+                },
+                template: async function (props) {
+                    let bidders = await props.bidders.data(numberOfBidders)
+                    let winners = await props.winners.data(numberOfBidders)
+                    return `
+                        <h3>${props.bidders.heading}</h3>
+                        <ul>
+                            ${bidders.map(function(bidder) {
+                                return `<li>${bidder.key} ${bidder.value}</li>`;
+                                }).join('')}
+                        </ul>
+                        <h3>${props.winners.heading}</h3>
+                        <ul>
+                            ${winners.map(function(winner) {
+                                return `<li>${winner.key} ${winner.value}</li>`;
+                                }).join('')}
+                        </ul>
+                        `;
+                }
+            }).render() 
+        });
     })
 }).catch(error => `Something went wrong while communicating with background script ${error.message}`)
